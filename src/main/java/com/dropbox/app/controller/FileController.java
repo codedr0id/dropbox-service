18b0ca65a2a002780aa6a54e6157a0ccf53222ac
Mehul Lathi <mehul.lathi@finmate.tech>
2024-09-06 19:59:35 +0530

    Added the Get File and List all Files APIs

--- src/main/java/com/dropbox/app/controller/FileController.java
+++ src/main/java/com/dropbox/app/controller/FileController.java
@@ -1,15 +1,22 @@
 package com.dropbox.app.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import org.springframework.http.HttpHeaders;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
 
 import com.dropbox.app.model.FileMetadata;
 import com.dropbox.app.service.FileService;
@@ -18,14 +25,35 @@ import com.dropbox.app.service.FileService;
 @RequestMapping("/files")
 public class FileController {
 
-	@Autowired
-	private FileService fileService;
+    @Autowired
+    private FileService fileService;
 
-	// Upload API
-	@PostMapping("/upload")
-	public ResponseEntity<FileMetadata> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-		FileMetadata metadata = fileService.storeFile(file);
-		return new ResponseEntity<>(metadata, HttpStatus.OK);
-	}
+    // Upload File API
+    @PostMapping("/upload")
+    public ResponseEntity<FileMetadata> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        FileMetadata metadata = fileService.storeFile(file);
+        return new ResponseEntity<>(metadata, HttpStatus.OK);
+    }
+
+    // GET File API
+    @GetMapping("/{fileId}")
+    public ResponseEntity<Resource> readFile(@PathVariable Long fileId)
+            throws FileNotFoundException, MalformedURLException {
+        Resource file = fileService.getFile(fileId);
+
+        FileMetadata metadata = fileService.getFileMetadata(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(metadata.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+    // LIST Files API
+    @GetMapping
+    public ResponseEntity<Iterable<FileMetadata>> listAllFiles() {
+        Iterable<FileMetadata> files = fileService.listAllFiles();
+        return new ResponseEntity<>(files, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/dropbox/app/service/FileService.java
+++ src/main/java/com/dropbox/app/service/FileService.java
@@ -1,7 +1,9 @@
 package com.dropbox.app.service;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
 import com.dropbox.app.model.FileMetadata;
 import com.dropbox.app.repository.FileMetadataRepository;
@@ -18,25 +22,41 @@ import com.dropbox.app.repository.FileMetadataRepository;
 @Service
 public class FileService {
 
-	@Autowired
-	private FileMetadataRepository fileMetadataRepository;
+    @Autowired
+    private FileMetadataRepository fileMetadataRepository;
 
-	@Value("${file.storage.path}")
-	private String storagePath;
+    @Value("${file.storage.path}")
+    private String storagePath;
 
-	public FileMetadata storeFile(MultipartFile file) throws IOException {
-		// Save the file locally
-		Path path = Paths.get(storagePath + File.separator + file.getOriginalFilename());
-		Files.write(path, file.getBytes());
+    public FileMetadata storeFile(MultipartFile file) throws IOException {
+        Path path = Paths.get(storagePath + File.separator + file.getOriginalFilename());
+        Files.write(path, file.getBytes());
 
-		// Save metadata to DB
-		FileMetadata metadata = new FileMetadata();
-		metadata.setFileName(file.getOriginalFilename());
-		metadata.setFileType(file.getContentType());
-		metadata.setSize(file.getSize());
-		metadata.setCreatedAt(LocalDateTime.now());
+        // Save metadata to DB
+        FileMetadata metadata = new FileMetadata();
+        metadata.setFileName(file.getOriginalFilename());
+        metadata.setFileType(file.getContentType());
+        metadata.setSize(file.getSize());
+        metadata.setCreatedAt(LocalDateTime.now());
 
-		return fileMetadataRepository.save(metadata);
-	}
+        return fileMetadataRepository.save(metadata);
+    }
+
+    public FileMetadata getFileMetadata(Long fileId) throws FileNotFoundException {
+        return fileMetadataRepository.findById(fileId)
+                .orElseThrow(() -> new FileNotFoundException("File not found with id: " + fileId));
+    }
+
+    public Resource getFile(Long fileId) throws FileNotFoundException, MalformedURLException {
+        FileMetadata metadata = fileMetadataRepository.findById(fileId)
+                .orElseThrow(() -> new FileNotFoundException("File not found"));
+
+        Path filePath = Paths.get(storagePath + File.separator + metadata.getFileName());
+        return new UrlResource(filePath.toUri());
+    }
+
+    public Iterable<FileMetadata> listAllFiles() {
+        return fileMetadataRepository.findAll();
+    }
 
 }
